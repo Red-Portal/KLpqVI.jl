9a7762b8cd94017470e03f1624ed99a574806456
DrWatson <no@mail>
2021-05-02 20:33:38 +0900

    add diagnoistics

--- src/vi.jl
+++ src/vi.jl
@@ -1,14 +1,4 @@
 
-function sample_variable(prng, q, model, varsym, n_samples)
-    vi      = DynamicPPL.VarInfo(model)
-    samples = map(1:n_samples) do i
-        z  = rand(prng, q)
-        DynamicPPL.setall!(vi, z)
-        vi.metadata[varsym].vals
-    end
-    hcat(samples...)
-end
-
 function pm_next!(pm, stat::NamedTuple)
     ProgressMeter.next!(pm; showvalues=[tuple(s...) for s in pairs(stat)])
 end
@@ -18,13 +8,6 @@ function sgd_step!(optimizer, θ::AbstractVector, ∇_buf)
     Δ = AdvancedVI.apply!(optimizer, θ, ∇)
     @. θ = θ - Δ
 end
-
-# function gen_∂logπ∂θ(vi, spl::DynamicPPL.AbstractSampler, model)
-#     function ∂logπ∂θ(x)
-#         return Turing.gradient_logp(x, vi, model, spl)
-#     end
-#     return ∂logπ∂θ
-# end
 
 function make_logjoint(rng, model::DynamicPPL.Model, weight::Real=1.0)
     # setup
@@ -56,8 +39,10 @@ function vi(model,
             optimizer,
             rng=Random.GLOBAL_RNG,
             callback=nothing,
-            sleep_freq=0,
+            sleep_interval=0,
             sleep_params=nothing,
+            rhat_interval=0,
+            paretok_samples=0,
             show_progress::Bool=false)
     varinfo     = DynamicPPL.VarInfo(model)
     varsyms     = keys(varinfo.metadata)
@@ -74,18 +59,30 @@ function vi(model,
         μ, σs = StatsBase.params(q_init)
         σ     = StatsFuns.invsoftplus.(σs)
     end
-    θ     = vcat(μ, σ)
-    q     = Turing.Variational.meanfield(model)
-    ∇_buf = DiffResults.GradientResult(θ)
+    θ        = vcat(μ, σ)
+    q        = Turing.Variational.meanfield(model)
+    ∇_buf    = DiffResults.GradientResult(θ)
 
-    init_state!(objective, rng, q, logπ, n_mc)
+    pimh_rhat_win = if(objective isa MSC_PIMH)
+        Array{Float64}(undef, rhat_interval, n_params, n_mc)
+    else
+        nothing
+    end
 
-    ws_aug = if (sleep_freq > 0)
+    rhat_win = if(rhat_interval > 0)
+        Array{Float64}(undef, rhat_interval, n_params*2)
+    else
+        nothing
+    end
+
+    ws_aug = if (sleep_interval > 0)
         z0_ws = rand(rng, q)
         WakeSleep(RV(z0_ws, logπ(z0_ws)), sleep_params)
     else
         nothing
     end
+
+    init_state!(objective, rng, q, logπ, n_mc)
 
     prog = if(show_progress)
         ProgressMeter.Progress(n_iter)
@@ -100,19 +97,43 @@ function vi(model,
         stat  = merge(stat, stat′)
         sgd_step!(optimizer, θ, ∇_buf)
 
-        if(sleep_freq > 0 && mod(t-1, sleep_freq) == 0)
+        if(sleep_interval > 0 && mod(t-1, sleep_interval) == 0)
             stat′ = sleep_phase!(rng, ws_aug, alg, q, logπ, ∇logπ, θ, ∇_buf)
             stat  = merge(stat, stat′)
             sgd_step!(optimizer, θ, ∇_buf)
         end
-
+        q′       = (q isa Distribution) ?  AdvancedVI.update(q, θ) : q(θ)
         elapsed  = Dates.now() - start_time
         stat     = merge(stat, (elapsed=elapsed,))
 
         if(!isnothing(callback))
-            q′    = (q isa Distribution) ?  AdvancedVI.update(q, θ) : q(θ)
             stat′ = callback(logπ, q′, objective, DiffResults.value(∇_buf))
             stat  = merge(stat, stat′)
+        end
+
+        if(rhat_interval > 0)
+            rhat_idx = mod(t-1, rhat_interval) + 1
+            rhat_win[rhat_idx,:] = θ
+
+            if(objective isa MSC_PIMH)
+                zs = hcat([z.val for z ∈ objective.zs]...)
+                pimh_rhat_win[rhat_idx,:,:] = zs
+            end
+
+            if(rhat_idx == rhat_interval)
+                R̂    = split_rhat(rhat_win)
+                stat = merge(stat, (rhat=maximum(R̂),))
+
+                if(objective isa MSC_PIMH)
+                    pimh_R̂ = split_rhat(pimh_rhat_win)
+                    stat   = merge(stat, (pimh_rhat=maximum(pimh_R̂),))
+                end
+            end
+        end
+
+        if(paretok_samples > 0)
+            k    = paretok(rng, q′, logπ, paretok_samples)
+            stat = merge(stat, (paretok=k,))
         end
 
         if(show_progress)
