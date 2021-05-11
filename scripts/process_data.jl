d3818d78d204bab7e31d16a79b7e673f5f29eb90
Khurai Kim <kyurai@cspro5.sogang.ac.kr>
2021-05-11 01:13:29 -0700

    fix distributed processing

--- scripts/process_data.jl
+++ scripts/process_data.jl
@@ -1,14 +1,16 @@
 
-using DrWatson
-@quickactivate "KLpqVI"
+            using Distributed
+@everywhere using DrWatson
+@everywhere @quickactivate "KLpqVI"
 
-using UnicodePlots
-using FileIO
-using JLD2
-using Statistics
-using ProgressMeter
+@everywhere using FileIO
+@everywhere using JLD2
+@everywhere using Statistics
+@everywhere using CSVFiles
+            using ProgressMeter
+@everywhere using DataFrames
 
-function process_data(ks, stats)::Dict
+@everywhere function process_data(ks, stats)::Dict
     mapreduce(merge, ks) do k
         filt_stats    = filter(stat -> k ∈ keys(stat), stats)
         y             = [stat[k] for stat ∈ filt_stats]
@@ -17,6 +19,11 @@ function process_data(ks, stats)::Dict
         if(k == :paretok)
             y[isinf.(y)] .= 1e+6
         end
+        if(length(y) > 1000)
+            y = y[1:10:end]
+            x = x[1:10:end]
+            t = t[1:10:end]
+        end
         name_y     = string(k) * "_y"
         name_x     = string(k) * "_x"
         name_t     = string(k) * "_t"
@@ -24,8 +31,8 @@ function process_data(ks, stats)::Dict
     end
 end
 
-function process_batches(batches, path)
-    allkeys = vcat(keys.(batches[1]))
+@everywhere function process_batches(batches, path)
+    allkeys = vcat(collect.(keys.(batches[1]))...)
     ks      = collect(Set(allkeys))
 
     @info "Finding y values"
@@ -34,7 +41,7 @@ function process_batches(batches, path)
     end
 
     @info "Computing statistics"
-    for k ∈ collect(ks)
+    for k ∈ ks
         name_y = string(k) * "_y"
         name_t = string(k) * "_t"
         name_x = string(k) * "_x"
@@ -52,26 +59,29 @@ function process_batches(batches, path)
         name_μ = string(k) * "_mean"
         μ_t    = mean(ts)
         μ_y    = mean(ys)
-        μtxμy  = hcat(μ_t, xs[1], μ_y)
-        FileIO.save(joinpath(path, name_μ*".csv"), μtxμy)
-
+        summ   = DataFrame("t" => ts[1],
+                           "x" => xs[1],
+                           "mean_t" => μ_t,
+                           "mean_y" => μ_y,
+                         )
         for i = 1:length(ys)
-            name = string(k)*string(i)*".csv"
-            txy  = hcat(ts[i], xs[i], ys[i])
-            FileIO.save(joinpath(path, name), txy)
+            insertcols!(summ, Symbol("y$(i)") => ys[i])
         end
+        name = string(k)*".csv"
+        FileIO.save(joinpath(path, name), summ)
     end
 end
 
 function main()
-    @showprogress map(datadir("exp_raw") |> readdir) do fname
+    @showprogress pmap(datadir("exp_raw") |> readdir) do fname
         data_name = fname[1:end-5]
         data      = FileIO.load(datadir("exp_raw", fname))
         result    = data["result"]
         settings  = data["settings"]
         task      = string(settings[:task])
         method    = string(settings[:method])
-        outpath   = datadir("exp_pro", task, method)
+        n_samples = string(settings[:n_samples])
+        outpath   = datadir("exp_pro", task, method, n_samples)
 
         @info "$(fname)" settings=merge(
             settings, Dict(:path=>outpath))
