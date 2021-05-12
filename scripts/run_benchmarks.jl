e823c5c5e6d46adc312ff5c551fca1aeca633931
DrWatson <no@mail>
2021-05-12 22:19:40 +0900

    add eightschools benchmark

--- scripts/run_benchmarks.jl
+++ scripts/run_benchmarks.jl
@@ -193,3 +193,47 @@ function gaussian_benchmarks()
         end
     end
 end
+
+function schools_benchmarks()
+    task      = "schools"
+    n_samples = 10
+    for settings ∈ [Dict(:method=>"MSC_PIMH",
+                         :task  =>task,
+                         :n_samples=>n_samples),
+
+                    Dict(:method=>"MSC_CIS",
+                         :task  =>task,
+                         :n_samples=>n_samples),
+
+                    Dict(:method=>"MSC_CISRB",
+                         :task  =>task,
+                         :n_samples=>n_samples),
+
+                    Dict(:method=>"SNIS",
+                         :task  =>task,
+                         :n_samples=>n_samples),
+
+                    Dict(:method=>"ELBO",
+                         :task  =>task,
+                         :n_samples=>n_samples),
+
+                    Dict(:method=>"RWS",
+                         :task  =>task,
+                         :n_samples=>n_samples),
+
+                    Dict(:method=>"MSC_CISRB_HMC",
+                         :task  =>task,
+                         :n_samples=>10),
+
+                    Dict(:method=>"MSC_PIMH_HMC",
+                         :task  =>task,
+                         :n_samples=>10),
+                    ]
+        @info "starting epxeriment" settings=settings
+        produce_or_load(datadir("exp_raw"),
+                        settings,
+                        run_experiment,
+                        suffix="jld2",
+                        loadfile=false)
+    end
+end
--- scripts/task/task.jl
+++ scripts/task/task.jl
@@ -11,6 +11,7 @@ using SparseArrays
 include("gaussian.jl")
 include("logistic.jl")
 #include("gmm.jl")
+include("eightschools.jl")
 include("sv.jl")
 include("lda.jl")
 include("lda_meanfield.jl")
