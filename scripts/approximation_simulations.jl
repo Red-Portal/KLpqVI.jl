c6cf96ae7ed069eaf216aa81bb567761b83e8297
DrWatson <no@mail>
2021-09-26 12:45:02 +0100

    add chain simulation

--- scripts/approximation_simulations.jl
+++ scripts/approximation_simulations.jl
@@ -2,38 +2,41 @@
 using DrWatson
 @quickactivate "KLpqVI"
 
-using Memoization
-using ReverseDiff
-using Plots, StatsPlots
-using Flux
-using ForwardDiff
-using Zygote
-using OnlineStats
-using Random123
-using ProgressMeter
-using DelimitedFiles
 using DataFrames
-using FileIO, JLD2
 using DataFramesMeta
+using DelimitedFiles
+using Distributed
+using Distributions
+using FileIO, JLD2
+using LinearAlgebra
+using Plots, StatsPlots
+using ProgressMeter
+using PDMats
+using Random
+using Random123
+using StatsFuns
+using Zygote
+using Base.Iterators
 
-include(srcdir("KLpqVI.jl"))
-include("task/task.jl")
-
-function kl(p, q)
-    σ₁ = p.σ
-    μ₁ = p.μ
-    σ₂ = q.σ
-    μ₂ = q.μ
-    log(σ₂ / σ₁) + (σ₁.^2 + (μ₁ - μ₂).^2)/(2*σ₂.^2) - 1/2
+function kl_divergence(p::MvNormal,
+                       q::MvNormal)
+    Σp = p.Σ
+    Σq = q.Σ
+    μp = p.μ
+    μq = q.μ
+    D  = length(μp)
+    ((logdet(Σq) - logdet(Σp)) - D
+    + tr(Σq \ Σp)
+    + PDMats.invquad(Σq, μq - μp) )/2
 end
 
 function cis(prng, zₜ₋₁, q, p, N)
-    z′ =  vcat(rand(prng, q, N), zₜ₋₁)
-    ℓw = logpdf.(Ref(p), z′) - logpdf.(Ref(q), z′)
+    z′ = hcat(rand(prng, q, N), zₜ₋₁)
+    ℓw = logpdf.(Ref(p), eachcol(z′)) - logpdf.(Ref(q), eachcol(z′))
     ℓZ = StatsFuns.logsumexp(ℓw)
     w  = exp.(ℓw .- ℓZ)
     w /= sum(w)
-    zₜ = rand(prng, Categorical(w))
+    zₜ = z′[:,rand(prng, Categorical(w))]
     r  = w[end]
     zₜ, r
 end
@@ -51,8 +54,8 @@ function imh(prng, zₜ₋₁, q, p)
 end
 
 function cisrb(prng, zₜ₋₁, q, p, N)
-    z′ = vcat(rand(prng, q, N), zₜ₋₁)
-    ℓw = logpdf.(p, z′) - logpdf.(q, z′)
+    z′ = hcat(rand(prng, q, N), zₜ₋₁)
+    ℓw = logpdf.(Ref(p), eachcol(z′)) - logpdf.(Ref(q), eachcol(z′))
     ℓZ = StatsFuns.logsumexp(ℓw)
     w  = exp.(ℓw .- ℓZ)
     w /= sum(w)
@@ -61,46 +64,126 @@ function cisrb(prng, zₜ₋₁, q, p, N)
 end
 
 function score(q, z)
-    Zygote.gradient(μ -> logpdf(Normal(μ, q.σ), z), q.μ)[1]
+    Zygote.gradient(μ -> logpdf(MvNormal(μ, q.Σ), z), q.μ)[1]
 end
 
-function cis_estimate(prng, zₜ₋₁, q, p, N)
-    zₜ, r = cis(prng, zₜ₋₁, q, p, N)
-    score(q, zₜ), r
-end
-
-function cisrb_estimate(prng, zₜ₋₁, q, p, N)
-    z′, w, r = cisrb(prng, zₜ₋₁, q, p, N)
-    dqdzs = score.(Ref(q), z′)
-    dot(dqdzs, w), r
-end
-
-function imh_estimate(prng, zₜ₋₁, q, p, N)
-    chains = imh.(Ref(prng), zₜ₋₁, Ref(q), Ref(p))
-    scores = [score(q, state[1]) for state ∈ chains]
-    mean(scores), 0
-end
-
-function run_simulation(prng, estimator, zₜ₋₁, p, q, N, name)
-    n_iters = 2^14
-    res     = map(1:n_iters) do i
-        estimator(prng, zₜ₋₁, q, p, N)   
+function cis_estimate(prng, q, p, p_z, N, n_iter)
+    z₀     = rand(prng, p_z)
+    n_dims = length(z₀)
+    scores = Array{Float64}(undef, n_dims, n_iter)
+    zₜ     = z₀
+    for t = 1:n_iter
+        q_z    = MvNormal(randn(n_dims), exp.(randn(n_dims) .+ 1))
+        zₜ, _  = cis(prng, zₜ, q_z, p, N)
+        ∇logqₜ =  score(q, zₜ)
+        scores[:,t] = ∇logqₜ
     end
-    scores   = [resᵢ[1] for resᵢ ∈ res]
-    rejrates = [resᵢ[2] for resᵢ ∈ res]
+    scores
+end
 
-    bias     = mean(scores) - p.μ
-    variance = var(scores)
-    klpq     = kl(p, q)
+function cisrb_estimate(prng, q, p, p_z, N, n_iter)
+    z₀     = rand(prng, p_z)
+    n_dims = length(z₀)
+    scores = Array{Float64}(undef, n_dims, n_iter)
+    zₜ     = z₀
+    for t = 1:n_iter
+        q_z         = MvNormal(randn(n_dims), exp.(randn(n_dims) .+ 1))
+        z′, w, _    = cisrb(prng, zₜ, q_z, p, N)
+        dqdzs       = hcat(score.(Ref(q), eachcol(z′))...)
+        zₜ          = z′[:,rand(prng, Categorical(w))]
+        scores[:,t] = dqdzs*w
+    end
+    scores
+end
 
-    Dict(:bias      => bias,
-         :variance  => variance,
-         :N         => N,
-         :kl        => klpq,
-         :rejection => mean(rejrates),
-         :q_mean    => q.μ,
-         :name      => name,
-         )
+function seqimh_estimate(prng, q, p, p_z, N, n_iter)
+    z₀     = rand(prng, p_z)
+    n_dims = length(z₀)
+    scores = Array{Float64}(undef, n_dims, n_iter)
+    zₜ     = z₀
+    zs     = Array{eltype(z₀)}(undef, n_dims, N)
+    for t = 1:n_iter
+        q_z    = MvNormal(randn(n_dims), exp.(randn(n_dims) .+ 1))
+        for i = 1:N
+            zₜ, _   = imh(prng, zₜ, q_z, p)
+            zs[:,i] = zₜ
+        end
+        scores[:,t] = mean(score.(Ref(q), eachcol(zs)))
+    end
+    scores
+end
+
+function parimh_estimate(prng, q, p, p_z, N, n_iter)
+    z₀s    = rand(prng, p_z, N)
+    n_dims = size(z₀s, 1)
+    scores = Array{Float64}(undef, n_dims, n_iter)
+    zₜs    = z₀s
+    for t = 1:n_iter
+        q_z = MvNormal(randn(n_dims), exp.(randn(n_dims) .+ 1))
+        for i = 1:N
+            z, _        = imh(prng, zₜs[:,i], q_z, p)
+            zₜs[:,i]    = z
+        end
+        scores[:,t] = mean(score.(Ref(q), eachcol(zₜs)))
+    end
+    scores
+end
+
+function score_expectation(p, q)
+    q.Σ \ (p.μ - q.μ)
+end
+
+function score_variance(scores)
+    diag_cov = var(scores)
+    norm(diag_cov, 1)
+end
+
+function run_simulation(prng, estimate, N, n_points, biased_init, name)
+    n_iter    = 20
+    n_dims    = 10
+    n_samples = 128
+    #n_samples = 32
+    q_μs      = randn(prng, n_dims, n_points)
+    q_logσs   = randn(prng, n_dims, n_points)
+    qs        = MvNormal.(eachcol(q_μs), eachcol(exp.(q_logσs)))
+    
+    p_μ = randn(prng, n_dims)
+    p_σ = exp.(randn(prng, n_dims))
+    p   = MvNormal(p_μ, p_σ)
+
+    μs    = randn(prng, n_dims, n_points)
+    logσs = randn(prng, n_dims, n_points)
+    q0s   = if biased_init
+        MvNormal.(eachcol(μs), eachcol(exp.(logσs)))
+    else
+        p
+    end
+
+    data = @showprogress pmap(enumerate(qs)) do (i, q)
+        samples = Array{Float64}(undef, n_dims, n_samples, n_iter)
+        for i = 1:n_samples
+            q0 = if biased_init
+                q0s[i]
+            else
+                q0s
+            end
+            samples[:,i,:] = estimate(prng, q, p, q0, N, n_iter)
+        end
+
+        E∇logq   = score_expectation(p, q)
+        bias     = mapslices(Δx -> norm(mean(Δx), 1), samples .- E∇logq, dims=(1,2))[1,1,:]
+        variance = mapslices(x  -> score_variance(x), samples,           dims=(1,2))[1,1,:]
+        klpq     = kl_divergence(p, q)
+
+        [Dict(:bias        => bias[t],
+              :variance    => variance[t],
+              :klpq        => klpq,
+              :N           => N,
+              :biased_init => biased_init,
+              :iteration   => t,
+              :name        => name) for t = 1:n_iter]
+    end
+    Iterators.flatten(data)
 end
 
 function main()
@@ -108,57 +191,40 @@ function main()
     prng = Random123.Philox4x(UInt64, seed, 8);
     Random123.set_counter!(prng, 0)
 
-    data = Dict[]
-    p    = Normal(0.0, 1.0)
+    # n_reps = 4
+    # d = Dict[]
+    # append!(d, run_simulation(prng, seqimh_estimate, 16, n_reps, false, :parimh))
+    # append!(d, run_simulation(prng, parimh_estimate, 16, n_reps, false, :seqimh))
+    # return DataFrame(d)
 
-    ProgressMeter.@showprogress for q_μ  ∈ collect(0.0:1.0:5.0)
-        q    = Normal(q_μ, 2.0)
-        for N ∈ [1, 2, 4, 8, 16, 32, 64, 128]
-            zₜ₋₁ = rand(prng, Normal(0.0, 8.0), N)
-            res  = run_simulation(prng, imh_estimate, zₜ₋₁, p, q, N, "pimh_diffuse")
-            push!(data, res)
+    #display(scatter( d1[:, :klpq], d1[:, :variance], xscale=:log10, yscale=:log10, alpha=0.5))
+    #display(scatter!(d2[:, :klpq], d2[:, :variance], xscale=:log10, yscale=:log10, alpha=0.5))
+    #return
+
+    n_rep = 2048
+
+    all_data = Dict[]
+    #for biased_init ∈ [true, false]
+    biased_init = false
+        for N ∈ [4, 8, 16]
+            data = run_simulation(prng, parimh_estimate, N, n_rep, biased_init, :parimh)
+            append!(all_data, data)
         end
-    end
 
-    ProgressMeter.@showprogress for q_μ  ∈ collect(0.0:1.0:5.0)
-        q    = Normal(q_μ, 2.0)
-        for N ∈ [1, 2, 4, 8, 16, 32, 64, 128]
-            zₜ₋₁ = rand(prng, p, N)
-            res  = run_simulation(prng, imh_estimate, zₜ₋₁, p, q, N, "pimh")
-            push!(data, res)
+        for N ∈ [4, 8, 16]
+            data = run_simulation(prng, parimh_estimate, N, n_rep, biased_init, :seqimh)
+            append!(all_data, data)
         end
-    end
 
-    zₜ₋₁ = [rand(prng, p)]
-    ProgressMeter.@showprogress for q_μ  ∈ collect(0.0:1.0:5.0)
-        q    = Normal(q_μ, 2.0)
-        for N ∈ [1, 2, 4, 8, 16, 32, 64, 128]
-            res = run_simulation(prng, cisrb_estimate, zₜ₋₁, p, q, N, "cisrb")
-            push!(data, res)
+        for N ∈ [4, 8, 16]
+            data = run_simulation(prng, cis_estimate, N, n_rep, biased_init, :cis)
+            append!(all_data, data)
         end
-    end
 
-    ProgressMeter.@showprogress for q_μ  ∈ collect(0.0:1.0:5.0)
-        q    = Normal(q_μ, 2.0)
-        for N ∈ [1, 2, 4, 8, 16, 32, 64, 128]
-            res = run_simulation(prng, cis_estimate, zₜ₋₁, p, q, N, "cis")
-            push!(data, res)
+        for N ∈ [4, 8, 16]
+            data = run_simulation(prng, cisrb_estimate, N, n_rep, biased_init, :cisrb)
+            append!(all_data, data)
         end
-    end
-    df     = DataFrame(data)
-    q_μs   = unique(df.q_mean)
-    names  = unique(df.name)
-
-    processed = Dict()
-    for name ∈ names
-        for q_μ ∈ q_μs
-            elem_name = "$(name)_$(q_μ)"
-            filtered  = @where(df, (:name .== name) .& (:q_mean .== q_μ))
-
-            processed[elem_name * "_x"]    = filtered.N
-            processed[elem_name * "_var"]  = filtered.variance
-            processed[elem_name * "_bias"] = filtered.bias
-        end
-    end
-    FileIO.save(datadir("exp_pro", "approximation_simulation.jld2"), processed)
+    #end
+    FileIO.save(datadir("exp_raw", "approximation_simulation_chain.jld2"), "data", DataFrame(all_data))
 end
