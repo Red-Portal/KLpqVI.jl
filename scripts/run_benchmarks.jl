03289686194f27ad6bc95c35f849991a071cc704
DrWatson <no@mail>
2021-10-12 03:45:53 +0100

    add elbo to bgp benchmarks

--- scripts/run_benchmarks.jl
+++ scripts/run_benchmarks.jl
@@ -19,12 +19,14 @@ using Distributed
 @everywhere include("task/task.jl")
 
 @everywhere function run_experiment(settings::Dict)
-    @unpack method, task, n_samples = settings
+    @unpack method, task, n_samples, n_reps = settings
 
     task = Val(Symbol(task))
     method, sleep_itvl = begin
         if(method== "MSC_PIMH")
             MSC_PIMH(), 0
+        elseif(method== "MSC_SIMH")
+            MSC_SIMH(), 0
         elseif(method == "ELBO")
             ELBO(), 0
         elseif(method == "MSC_CIS")
@@ -44,10 +46,9 @@ using Distributed
             SNIS(), 0
         end
     end
-    sleep_ϵ, sleep_L = hmc_params(task)
+    sleep_ϵ, sleep_L = 0.1, 16#hmc_params(task)
 
-    n_iters = 100
-    stats   = ProgressMeter.@showprogress pmap(1:n_iters) do seed_key
+    stats   = ProgressMeter.@showprogress pmap(1:n_reps) do seed_key
         seed = (0x97dcb950eaebcfba, 0x741d36b68bef6415)
         prng = Random123.Philox4x(UInt64, seed, 8)
         Random123.set_counter!(prng, seed_key)
@@ -65,11 +66,12 @@ using Distributed
 end
 
 function general_benchmarks()
-    for task ∈ ["pima",
-                "heart",
-                "ionosphere",
-                "german",
-                "sonar"]
+    for task ∈ [#"pima",
+                #"heart",
+                #"ionosphere",
+                #"german",]
+                #"sonar"
+                ]
         for settings ∈ [Dict(:method=>"MSC_PIMH",
                              :task  =>task,
                              :n_samples=>10),
@@ -82,17 +84,25 @@ function general_benchmarks()
                              :task  =>task,
                              :n_samples=>10),
 
-                        Dict(:method=>"MSC_CISRB_HMC",
+                        Dict(:method=>"MSC_SIMH",
                              :task  =>task,
                              :n_samples=>10),
 
-                        Dict(:method=>"MSC_PIMH_HMC",
-                             :task  =>task,
-                             :n_samples=>10),
+                        # Dict(:method=>"MSC_HMC",
+                        #      :task  =>task,
+                        #      :n_samples=>10),
 
-                        Dict(:method=>"RWS",
-                             :task  =>task,
-                             :n_samples=>10),
+                        # Dict(:method=>"MSC_CISRB_HMC",
+                        #      :task  =>task,
+                        #      :n_samples=>10),
+
+                        # Dict(:method=>"MSC_PIMH_HMC",
+                        #      :task  =>task,
+                        #      :n_samples=>10),
+
+                        # Dict(:method=>"RWS",
+                        #      :task  =>task,
+                        #      :n_samples=>10),
 
                         Dict(:method=>"SNIS",
                              :task  =>task,
@@ -102,89 +112,114 @@ function general_benchmarks()
                              :task  =>task,
                              :n_samples=>10),
                         ]
-
             @info "starting epxeriment" settings=settings
+            settings[:n_reps] = 100
             produce_or_load(datadir("exp_raw"),
-                                    settings,
-				    run_experiment,
-				    suffix="jld2",
-				    loadfile=false)
+                            settings,
+                            run_experiment,
+                            suffix="jld2",
+                            loadfile=false,
+                            tag=false)
         end
     end
 
     n_samples = 10
-    for task ∈ ["sv", "radon"]
-        for settings ∈ [Dict(:method=>"MSC_PIMH",
-                             :task  =>task,
-                             :n_samples=>n_samples),
+    for task ∈ ["sv"]#, "radon"]
+        for settings ∈ [# Dict(:method=>"MSC_PIMH",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
 
-                        Dict(:method=>"MSC_CIS",
-                             :task  =>task,
-                             :n_samples=>n_samples),
+                        # Dict(:method=>"MSC_SIMH",
+                        #     :task  =>task,
+                        #     :n_samples=>n_samples),
 
-                        Dict(:method=>"MSC_CISRB",
-                             :task  =>task,
-                             :n_samples=>n_samples),
+                        # Dict(:method=>"MSC_CIS",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
 
-                        Dict(:method=>"SNIS",
-                             :task  =>task,
-                             :n_samples=>n_samples),
+                        # Dict(:method=>"MSC_CISRB",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
+
+                        # Dict(:method=>"SNIS",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
 
                         # Dict(:method=>"RWS",
                         #      :task  =>task,
                         #      :n_samples=>n_samples),
 
-                        Dict(:method=>"ELBO",
-                             :task  =>task,
-                             :n_samples=>n_samples),
+                        # Dict(:method=>"ELBO",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
+
+                        # Dict(:method=>"MSC_HMC",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
+
                         ]
+            settings[:n_reps] = 100
             @info "starting epxeriment" settings=settings
             produce_or_load(datadir("exp_raw"),
                             settings,
                             run_experiment,
                             suffix="jld2",
-                            loadfile=false)
+                            loadfile=false,
+                            tag=false)
         end
     end
-end
 
-function colon_benchmarks()
     n_samples = 10
-    task      = "colon"
-    for settings ∈ [Dict(:method=>"MSC_PIMH",
-                         :task  =>task,
-                         :n_samples=>n_samples),
+    for task ∈ ["sonar", "ionosphere", "breast"]
+        for settings ∈ [Dict(:method=>"MSC_PIMH",
+                             :task  =>task,
+                             :n_samples=>n_samples),
 
-                    Dict(:method=>"MSC_CIS",
-                         :task  =>task,
-                         :n_samples=>n_samples),
+                        Dict(:method=>"MSC_SIMH",
+                            :task  =>task,
+                            :n_samples=>n_samples),
 
-                    Dict(:method=>"MSC_CISRB",
-                         :task  =>task,
-                         :n_samples=>n_samples),
+                        Dict(:method=>"MSC_CIS",
+                             :task  =>task,
+                             :n_samples=>n_samples),
 
-                    Dict(:method=>"SNIS",
-                         :task  =>task,
-                         :n_samples=>n_samples),
+                        Dict(:method=>"MSC_CISRB",
+                             :task  =>task,
+                             :n_samples=>n_samples),
 
-                    Dict(:method=>"ELBO",
-                         :task  =>task,
-                         :n_samples=>n_samples),
-                    ]
-        @info "starting epxeriment" settings=settings
+                        Dict(:method=>"SNIS",
+                             :task  =>task,
+                             :n_samples=>n_samples),
 
-        run_experiment_colon(settings) = begin
-            dict           = run_experiment(settings)
-            res            = dict["result"]
-            dict["result"] = [resᵢ[1] for resᵢ ∈ res]
-            dict["beta"]   = [resᵢ[2] for resᵢ ∈ res]
-            dict
+                        # Dict(:method=>"ELBO",
+                        #      :task  =>task,
+                        #      :n_samples=>n_samples),
+                        ]
+            settings[:n_reps] = 30
+            @info "starting epxeriment" settings=settings
+            produce_or_load(datadir("exp_raw"),
+                            settings,
+                            run_experiment,
+                            suffix="jld2",
+                            loadfile=false,
+                            tag=false)
         end
-        produce_or_load(datadir("exp_raw"),
-                        settings,
-                        run_experiment_colon,
-                        suffix="jld2",
-                        loadfile=false)
+    end
+
+    n_samples = 10
+    for task ∈ ["sonar", "ionosphere", "breast"]
+        for settings ∈ [Dict(:method=>"ELBO",
+                             :task  =>task,
+                             :n_samples=>n_samples),]
+            settings[:n_reps] = 30
+            @info "starting epxeriment" settings=settings
+            produce_or_load(datadir("exp_raw"),
+                            settings,
+                            run_experiment,
+                            suffix="jld2",
+                            loadfile=false,
+                            tag=false)
+        end
     end
 end
 
@@ -207,14 +242,6 @@ function gaussian_benchmarks()
                                  :task  =>task,
                                  :n_samples=>n_samples),
 
-                            Dict(:method=>"MSC_CISRB_HMC",
-                                 :task  =>task,
-                                 :n_samples=>n_samples),
-
-                            Dict(:method=>"MSC_PIMH_HMC",
-                                 :task  =>task,
-                                 :n_samples=>n_samples),
-
                             Dict(:method=>"RWS",
                                  :task  =>task,
                                  :n_samples=>n_samples),
@@ -226,11 +253,30 @@ function gaussian_benchmarks()
                 @info "starting epxeriment" settings=settings
 
                produce_or_load(datadir("exp_raw"),
-                                    settings,
-				    run_experiment,
-				    suffix="jld2",
-				    loadfile=false)
+                               settings,
+                               run_experiment,
+                               suffix="jld2",
+                               loadfile=false,
+                               tag=false)
             end
         end
     end
 end
+
+# function main()
+#     seed = (0x97dcb950eaebcfba, 0x741d36b68bef6415)
+#     prng = Random123.Philox4x(UInt64, seed, 8)
+#     Random123.set_counter!(prng, seed_key)
+
+#     Random.seed!(seed_key)
+#     run_task(prng,
+#                  task,
+#                  method,
+#                  n_samples,
+#                  sleep_itvl,
+#                  sleep_ϵ,
+#                  sleep_L;
+#                  show_progress=false)
+#     end
+#     Dict("result"=>stats, "settings"=>settings)
+# end
