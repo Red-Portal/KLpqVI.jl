9e27210e7b9de49ab8361bbd4f52cf2f23b0eee3
DrWatson <no@mail>
2022-04-24 02:16:16 +0100

    update task loading

--- scripts/task/task.jl
+++ scripts/task/task.jl
@@ -3,6 +3,11 @@ using ParserCombinator
 using SparseArrays
 using JLD2
 
+using CUDA
+using KernelAbstractions
+using CUDAKernels
+
+import DistributionsAD
 import FileIO
 import Distances
 import JLD
@@ -12,9 +17,10 @@ import Random
 import PyCall
 import AdvancedHMC
 
+include("gp_utils.jl")
 include("gaussian.jl")
 include("logistic.jl")
 include("eightschools.jl")
 include("radon.jl")
-include("bgp.jl")
+include("lgp.jl")
 include("bnn.jl")
